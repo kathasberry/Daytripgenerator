670a1757faeb2e6e53d85a9fee02a34140067258
kathasberry <kat_hasberry@outlook.com>
2022-08-14 20:04:27 -0400

    added the lists for each part of  the trip

--- daytrip_user_story.py
+++ daytrip_user_story.py
@@ -1,3 +1,9 @@
-day_trip = [places_to_go, things_to_eat, gettting_there, fun_fun-fun]
+day_trip = [places_to_go, things_to_eat, gettting_there, fun_fun_fun]
 
 places_to_go = ['New york', 'Seattle', 'New Orleans', 'Houston']
+
+things_to_eat = ['Eleven Madison park', 'Sushi Kashiba', 'GW Fins', 'State of Grace']
+
+getting_there = ['Taxi', 'Double Decker bus', 'Train', 'Plane']
+
+fun_fun_fun = ['Escape the room', 'Portal Virtual Reality', 'Audubon Aquarium', "Houston Funplex"]
